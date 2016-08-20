1db3bb551f86dfaaa2a43e2c41e3baa7708cae4f
Anthony Schoeters <anthony.schoeters@hotmail.com>
2016-08-20 16:11:10 +0200

    data loading

--- SNA script R.R
+++ SNA script R.R
@@ -1,7 +1,13 @@
 
+
 setwd("C:/Users/antho/Documents/KU Leuven/Master HIRb/Thesis")
 getwd()
 options(max.print=999999)
+
+mydata1 = read.csv("article_dataset_thesis.csv", header = TRUE, sep= ";", quote = "\"")
+mydata1
+summary(mydata1)
+
 mydata2 = read.csv("user_data.csv", header = TRUE, sep=",")
 mydata2
 summary(mydata2)
